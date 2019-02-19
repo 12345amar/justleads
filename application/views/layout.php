4e22be657f6dcaa63d4a58e48c3ea372f38d89ba
12345amar <ameriki2012@gmail.com>
2019-02-19 12:38:22 +0530

    layout

--- application/views/layout.php
+++ application/views/layout.php
@@ -4,18 +4,7 @@ include('include/header.php');
 
 include('include/left_side_bar.php');
 
-if ($this->router->fetch_class() == 'telecaller') {  
-   
-    include('talle_caller/'.$page.'.php'); 
-}
-
-if ($this->router->fetch_class() == 'admin') {    
-    include('admin/'.$page.'.php');
-}
-
-if ($this->router->fetch_class() == 'user') {    
-    include('user/'.$page.'.php'); 
-}
+include('talle_caller/'.$page.'.php'); 
 
 include('include/footer.php'); 
 
